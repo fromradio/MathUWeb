277ffa646bab259dfd77b08f7d9b056e1a2872da
Ruimin Wang <ruimin.wang13@gmail.com>
2015-01-07 18:28:20 +0800

    index

--- MathUWeb/urls.py
+++ MathUWeb/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'MathUWeb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^',include('index.urls',namespace="mathu")),
+    url(r'^polls/', include('polls.urls',namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from django.http import HttpResponse
 
-# Create your views here.
+from polls.models import Poll
+
+
+def index(request):
+	latest_question_list = Poll.objects.order_by('-pub_date')[:5]
+	context = {'latest_question_list':latest_question_list}
+	return render(request,'polls/index.html',context)
+	
+def detail(request, question_id):
+	question = get_object_or_404(Poll,pk=question_id)
+	return render(request,'polls/detail.html',{'question':question})
+
+def results(request, question_id):
+	question = get_object_or_404(Poll,pk=question_id)
+	return render(request,'polls/results.html',{'question':question})
+
+def vote(request, question_id):
+	question = get_object_or_404(Poll,pk=question_id)
+	return render(request,'polls/vote.html',{'question':question})
