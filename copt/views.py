8bafc0873c7ff7e0b0a19a9c390a4c41b5844419
Ruimin Wang <ruimin.wang13@gmail.com>
2015-01-12 17:34:05 +0800

    Choose to use html

--- copt/views.py
+++ copt/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from copt.models import Chapter
+import os.path
 
 def index(request):
 	chapters = Chapter.objects.all()
@@ -9,4 +10,14 @@ def chapter(request,chapter_name):
 	chap = get_object_or_404(Chapter,name=chapter_name)
 	item_list = chap.item_set.all()
 	introduction = chap.introduction_set.all()
-	return render(request,'copt/chapter.html',{'intro_list':introduction,'item_list':item_list,'chapter':chap})
+	chap_name = 'copt/templates/copt/'+chapter_name+'.html'
+	if(os.path.exists(chap_name)):
+		chap_name='copt/'+chapter_name+'.html'
+		return render(request,chap_name,{'chapter':chap})
+	else:
+		return render(request,'copt/chapter.html',{'intro_list':introduction,'item_list':item_list,'chapter':chap})
+def test():
+	chapter_name = 'VectorBase'
+	chap_name = 'copt/templates/copt/'+chapter_name+'.html'
+	print chap_name
+	print os.path.exists(chap_name)
